7fc8e992e87548e46a1f17ecc82de9c4a029da9c
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2020-01-22 15:39:01 +0200

    coming back

--- challenges/array-binary-search.js
+++ challenges/array-binary-search.js
@@ -1,18 +1,34 @@
 'use strict';
 
-// let sortdArray = [4,8,15,16,23,42], searchKey = 15;
+let sortedArray = [4,8,15,16,23,42], searchKey = 15;
 // let sa = [11,22,33,44,55,66,77], skey = 90;
 
+// function binarySearch(arr, searchKey){
+//   let indexOfSearchKey = -1;
+//   for(let i = 0; i < arr.length ; i++){
+//     if(searchKey == arr[i]){
+//       indexOfSearchKey = i;
+//     }
+//   }
+//   return indexOfSearchKey;
+// }
+
+// console.log(binarySearch(sortedArray, searchKey));
+
+
+
 function binarySearch(arr, searchKey){
-  let indexOfSearchKey = -1;
-  for(let i = 0; i < arr.length ; i++){
-    if(searchKey == arr[i]){
-      indexOfSearchKey = i;
-    }
-  }
-  return indexOfSearchKey;
+    L := 0
+    R := n
+    while L < R:
+        m := floor((L + R) / 2)
+        if A[m] > T:
+            R := m
+        else:
+            L := m + 1
+    return L - 1
 }
 
-// console.log(binarySearch(sa, skey));
+console.log(binarySearch(sortedArray, searchKey));
 
 module.exports = binarySearch;
