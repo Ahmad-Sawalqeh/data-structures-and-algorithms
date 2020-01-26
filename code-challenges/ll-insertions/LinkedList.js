68a19b455e68b38673d2785a9371fa1aeb285a7e
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2020-01-26 22:13:12 +0200

    done

---
+++ code-challenges/ll-insertions/LinkedList.js
@@ -0,0 +1,92 @@
+'use strict';
+
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    const node = new Node(value, null);
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+    let currentNode = this.head;
+    while(currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    return this;
+  }
+
+  insert(value) {
+    const node = new Node(value, null);
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+    let currentNode = this.head;
+    this.head = node;
+    node.next = currentNode;
+    return this;
+  }
+
+  includes(value) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) return true;
+      currentNode = currentNode.next;
+    }
+    return false;
+  }
+
+  toString(){
+    let nodeList = [], currentNode = this.head;
+    while (currentNode) {
+      nodeList.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return nodeList.map(val => `{ ${val} }`).join(' --> ').concat(` --> NULL`);
+  }
+
+  insertBefore(value, newVal){
+    const node = new Node(newVal, null);
+    let currentNode = this.head, previousNode = currentNode;
+    while (currentNode) {
+      if(currentNode.value === value){
+        node.next = currentNode;
+        // currentNode = node;
+        previousNode.next = node;
+        break;
+      }else{
+        previousNode = currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+
+  insertAfter(value, newVal){
+    const node = new Node(newVal, null);
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        node.next = currentNode.next;
+        currentNode.next = node;
+        break;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+
+}
+
+
+module.exports = LinkedList;
---
+++ code-challenges/ll-insertions/linkedList.test.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const LinkedList = require('./LinkedList.js');
+
+let list;
+
+beforeEach(() => {
+  list = new LinkedList();
+});
+
+describe('Linked List Module', () => {
+
+  it('Can successfully instantiate an empty linked list', () => {
+    expect(list.head).toBeNull();
+  });
+
+  it('Can properly insert into the linked list', () => {
+    let firstVal = 'string';
+    list.insert(firstVal);
+    expect(list.head.value).toEqual(firstVal);
+  });
+
+  it('Can successfully add a node to the end of the linked list', () => {
+    let node1 = 'string',node2 = 'number', node3 = 'array';
+    list.insert(node1);
+    list.insert(node2);
+    list.insert(node3);
+    expect(list.head.value).toEqual(node3);
+  });
+
+  it('Can successfully add multiple nodes into the linked listand at the end of a linked list', () => {
+    let node1 = 'string',node2 = 'number', node3 = 'array';
+    list.append(node1);
+    list.append(node2);
+    list.append(node3);
+    expect(list.toString()).toEqual(`{ ${node1} } --> { ${node2} } --> { ${node3} } --> NULL`);
+  });
+
+  it('Can successfully insert a node before a node located i the middle of a linked list', () => {
+    list.append(8);
+    list.append(2);
+    list.append(7);
+    list.append(6);
+    list.insertBefore(2,100);
+    expect(list.head.next.value).toEqual(100);
+  });
+
+  // it('Can successfully insert a node before the first node of a linked list', () => {
+  //   list.append(8);
+  //   list.append(2);
+  //   list.append(7);
+  //   list.append(6);
+  //   list.insertBefore(8,100);
+  //   expect(list.head.value).toEqual(100);
+  // });
+
+  it('Can successfully insert after a node in the middle of the linked list', () => {
+    list.insert(67);
+    list.insert(25);
+    list.insert(6);
+    list.insertAfter(25,100);
+    expect(list.head.next.next.value).toEqual(100);
+  });
+
+});
