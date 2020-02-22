681a52824c7f4a611af1bc7f222cbd0f1f90ec37
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2020-02-22 12:13:23 +0200

    done

--- code-challenges/fizzbuzz-tree/fizz-buzz-tree.js
+++ code-challenges/fizzbuzz-tree/fizz-buzz-tree.js
@@ -46,8 +46,24 @@ class BinaryTree {
 
 }
 
-function FizzBuzzTree(){
+function fizzBuzzTree(tree){
+
+  let _walk = (node) => {
+    if(node.value % 3 === 0 && node.value % 5 === 0){
+      node.value = 'FizzBuzz';
+    }else if(node.value % 3 === 0){
+      node.value = 'Fizz';
+    }else if(node.value % 5 === 0){
+      node.value = 'Buzz';
+    }else{
+      node.value = `${node.value}`;
+    }
+    if(node.left) _walk(node.left);
+    if(node.right) _walk(node.right);
+  };
+  _walk(tree);
+  return tree ;
 
 }
 
-// module.exports = functio;
+module.exports = { Node, BinaryTree, fizzBuzzTree, };
