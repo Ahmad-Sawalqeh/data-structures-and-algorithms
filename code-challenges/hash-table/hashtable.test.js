cb158940c135da8af4471c266b2044dbf5ca5a67
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2020-02-27 10:14:45 +0200

    need work

---
+++ code-challenges/hash-table/hashtable.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const hashTable = require('./hashtable.js');
+
+describe('testing hash table datastructur',() => {
+
+  it('Adding a key/value to your hashtable results in the value being in the data structure', () => {
+
+  });
+
+  it('Retrieving based on a key returns the value stored',() => {
+
+  });
+
+  it('Successfully handle a collision within the hashtable',() => {
+
+  });
+
+  it('Successfully retrieve a value from a bucket within the hashtable that has a collision',() => {
+
+  });
+  it('Successfully hash a key to an in-range value',() => {
+
+  });
+
+});
