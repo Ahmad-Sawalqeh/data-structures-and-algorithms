3e11a565cc026e209f6e227b39151fb44c34367f
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2020-03-01 16:29:05 +0200

    working on

---
+++ code-challenges/repeatedWord/repeated-word.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function repeatedWord(string){
+
+}
+
+let string = `Once upon a time, there was a brave princess who...`;
+
+console.log('repeatedWord => ',repeatedWord(string));
+
+module.exports = repeatedWord;
---
+++ code-challenges/repeatedWord/repeated-word.test.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const repeatedWord = require('./repeated-word.js');
+
