d62fc5053af84520afdd14676ecd49a9792e5ae1
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2020-03-06 13:12:24 +0200

    done

--- code-challenges/leftJoin/leftJoin.js
+++ code-challenges/leftJoin/leftJoin.js
@@ -1,9 +1,21 @@
 'use strict';
 
+// const { Node, LinkedList, Hashmap, } = require('./hash-table.js');
+
 function leftJoin(table1, table2){
 
-}
+  let result = [];
+  let keys = Object.keys(table1);
+  // let keys2 = Object.keys(table2);
 
-console.log('leftJoin => ',leftJoin(table1, table2));
+  for (let i = 0; i < keys.length; i++) {
+    if (table2[keys[i]]) {
+      result.push([keys[i], table1[keys[i]], table2[keys[i]]]);
+    } else {
+      result.push([keys[i], table1[keys[i]], null]);
+    }
+  }
+  return result;
+}
 
 module.exports = leftJoin;
--- code-challenges/leftJoin/leftJoin.test.js
+++ code-challenges/leftJoin/leftJoin.test.js
@@ -2,3 +2,45 @@
 
 const leftJoin = require('./leftJoin.js');
 
+describe('Left-Join', ()=> {
+
+  let a ;
+  let b ;
+  beforeEach(()=> {
+    a = {
+      'fond': 'enamored',
+      'wrath': 'anger',
+      'diligent': 'employed',
+      'outfit' : 'garb',
+      'guide': 'usher',
+    };
+
+    b = {
+      'fond': 'averse',
+      'wrath': 'delight',
+      'diligent': 'idle',
+      'guide': 'follow',
+      'flow' : 'jam',
+    };
+  });
+
+  it('return keys of table1',()=>{
+    let test = leftJoin(a, b);
+    Object.keys(a).map((val, idx) => {
+      expect(test[idx].includes(a[val])).toBeTruthy();
+    });
+  });
+
+
+  it('return null if key not exist in table2',()=>{
+    let test = leftJoin(a, b);
+    expect(test[3].includes(null)).toBeTruthy();
+  });
+
+
+  it('adding values for repeated keys',()=>{
+    let test = leftJoin(a, b);
+    expect(test[0].includes('averse')).toBeTruthy();
+  });
+
+});
